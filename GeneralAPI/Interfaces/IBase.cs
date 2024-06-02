4c2bc8e110884da2f56713a1f3ee3deb509ce067
Samuel Charles <samyrd309@gmail.com>
2024-06-01 23:41:02 -0400

    v.1

---
+++ GeneralAPI/Interfaces/IBase.cs
@@ -0,0 +1,7 @@
+
+namespace GeneralAPI.Interfaces;
+
+public interface IBase
+{
+    public int Id {get;}
+}
---
+++ GeneralAPI/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+namespace GeneralAPI.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(int id);
+
+}
